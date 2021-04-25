6b77002766d69a7210258206df04a68e2b01b787
GhostlyDark <31742919+GhostlyDark@users.noreply.github.com>
2021-04-25 12:25:50 +0200

    Add Dutch and Swedish translations

--- _cache_service_worker.js
+++ _cache_service_worker.js
@@ -29,7 +29,7 @@ caches.keys().then(function(cacheNames){
 });
 
 var PRECACHE_ID='rom-patcher-js';
-var PRECACHE_VERSION='v18';
+var PRECACHE_VERSION='v19';
 var PRECACHE_URLS=[
 	'rom-patcher-js.htm',
 	'manifest.json',
